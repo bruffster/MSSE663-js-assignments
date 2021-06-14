df5dde31123f87874ae342c1ff0f9e95797e3b0f
Benjamin Bruffey <bruffster@gmail.com>
2021-06-14 14:16:33 -0600

    changed which function was commented and unit test

--- week6/src/higher-order-funcs.spec.ts
+++ week6/src/higher-order-funcs.spec.ts
@@ -4,15 +4,15 @@ import 'mocha';
 import { filterApplied, mappedThings } from './higher-order-funcs';
 
 describe('Higher Order Function tests', () => {
-  it("#1 should produce the expected result `[{ id: 1, name: 'thing' }, { id: 2, name: 'thing' }]` after hoisting", () => {
-    //commented out for if the thing should have the last character sliced
-    //expect(mappedThings).to.eql([{ id: 1, name: 'thing1' }, { id: 2, name: 'thing2' }]);
+  it("#1 should produce the expected result `[{ id: 1, name: 'thing1' }, { id: 2, name: 'thing2' }]` after hoisting", () => {
+    //if it should be thing1 and thing2
+    expect(mappedThings).to.eql([{ id: 1, name: 'thing1' }, { id: 2, name: 'thing2' }]);
 
     //test for if last character is sliced
-    expect(mappedThings).to.eql([{ id: 1, name: 'thing' }, { id: 2, name: 'thing' }]);
+    //expect(mappedThings).to.eql([{ id: 1, name: 'thing' }, { id: 2, name: 'thing' }]);
   });
 
   it('#2 should return a single thing from array of things objects after hoisting and currying', () => {
-    expect(filterApplied('2')).to.eql({ id: 2, name: 'thing' });
+    expect(filterApplied('2')).to.eql({ id: 2, name: 'thing2' });
   });
 });
