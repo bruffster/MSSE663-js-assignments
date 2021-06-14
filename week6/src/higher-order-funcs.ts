7b4d145d90e2b4caf9f4772d179459868ceec7a4
Benjamin Bruffey <bruffster@gmail.com>
2021-06-14 14:16:33 -0600

    changed which function was commented and unit test

--- week6/src/higher-order-funcs.ts
+++ week6/src/higher-order-funcs.ts
@@ -13,14 +13,14 @@ const things = ['thing1', 'thing2'];
 // create callback here
 
 // callback for if thing should actually be 'thing1' and 'thing2' in the pseduo code
-//export const callback = (thing: string, id: number) => ({id: Number(thing.slice(-1).valueOf()), name: thing});
+export const callback = (thing: string, id: number) => ({id: id + 1, name: thing});
 
 // callback for if thing should be 'thing' and 'thing' in the mapped result, .slice(0,-1) removes the last character from the string, .slice(-1) gets last character which is used as the id
-export const callback = (thing: string) => ({id: Number(thing.slice(-1)), name: thing.slice(0,-1)});
+//export const callback = (thing: string) => ({id: Number(thing.slice(-1)), name: thing.slice(0,-1)});
 
 // create map here
 export const mappedThings = things.map(callback);
-//console.log(mappedThings);
+console.log(mappedThings);
 
 /**
  * #2 Higher order then curry.
